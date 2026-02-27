f82ab33dd782d5182fae91ef5152d5137fec731e
jakaprayudha <jakaprayudha3@gmail.com>
2026-02-27 07:13:59 +0700

    fix:print template test urine

--- app/template/template2.php
+++ app/template/template2.php
@@ -1,26 +1,63 @@
+<?php
+
+$time = date(
+  "d-m-Y H:i",
+  strtotime(($pasien['tanggal'] ?? '') . ' ' . ($pasien['waktu'] ?? ''))
+);
+
+function rowUrine($label, $value)
+{
+  if ($value === '' || $value === null) return '';
+
+  return sprintf("%-5s %-10s\n", $label, $value);
+}
+
+function rowUrineWide($label, $value)
+{
+  if ($value === '' || $value === null) return '';
+
+  // lebih menjorok ke kanan seperti alat
+  return sprintf("%-5s %12s\n", $label, $value);
+}
+
+?>
 <!doctype html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Urine Sample Report</title>
-    <link rel="stylesheet" href="urine.css" />
-  </head>
 
-  <body>
-    <pre class="struk">
-Date:07-00-2080        22:43
-No.  0683 
-UBG   Normal  3.4umol/L
-BIL   Neg
-KET   Neg
-BLD   Neg
-*PRO  Trace
-NIT   Neg
-LEU   Neg
-GLU   Neg
-SG           1.025
-pH           6.0
-</pre
-    >
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <title>Urine Sample Report</title>
+  <link rel="stylesheet" href="../app/template/urine.css" />
+</head>
+
+<body>
+
+  <pre class="struk">
+Date:<?= $time ?>        
+No.  <?= $pasien['nopermintaan'] ?? '-' ?> 
+
+<?= rowUrine("UBG", $hasil['ubg'] ?? '') ?>
+<?= rowUrine("BIL", $hasil['bil'] ?? '') ?>
+<?= rowUrine("KET", $hasil['ket'] ?? '') ?>
+<?= rowUrine("BLD", $hasil['bld'] ?? '') ?>
+<?= rowUrine("PRO", $hasil['pro'] ?? '') ?>
+<?= rowUrine("NIT", $hasil['nit'] ?? '') ?>
+<?= rowUrine("LEU", $hasil['leu'] ?? '') ?>
+<?= rowUrine("GLU", $hasil['glu'] ?? '') ?>
+<?= sprintf("%-5s %15s\n", "SG", $hasil['sg'] ?? '') ?>
+<?= sprintf("%-5s %15s\n", "pH", $hasil['ph'] ?? '') ?>
+
+</pre>
+
+  <script>
+    window.onload = () => {
+      setTimeout(() => {
+        window.print();
+        window.close();
+      }, 500);
+    };
+  </script>
+
+</body>
+
 </html>
