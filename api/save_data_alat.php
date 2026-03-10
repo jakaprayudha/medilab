abf0fc5d8157b7f082684727ed4217274ac7ef0d
jakaprayudha <jakaprayudha3@gmail.com>
2026-03-10 09:03:52 +0700

    feat:update condition results exiting data

--- api/save_data_alat.php
+++ api/save_data_alat.php
@@ -2,6 +2,7 @@
 require_once "../database/db.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
+
 $permintaan = $data['permintaan'];
 $lab = $data['lab'];
 $list = $data['data'];
@@ -13,21 +14,54 @@ foreach ($list as $item) {
    $satuan = $item['satuan'];
    $referensi = $item['referensi'];
 
-   $stmt = $conn->prepare("
-        INSERT INTO hasil_lab
-        (permintaan,lab,parameter,hasil,satuan,referensi,create_at)
-        VALUES (?,?,?,?,?,?,NOW())
+   // cek apakah data sudah ada
+   $cek = $conn->prepare("
+        SELECT id 
+        FROM hasil_lab 
+        WHERE permintaan=? AND lab=? AND parameter=?
     ");
 
-   $stmt->bind_param(
-      "ssssss",
-      $permintaan,
-      $lab,
-      $parameter,
-      $hasil,
-      $satuan,
-      $referensi
-   );
+   $cek->bind_param("sss", $permintaan, $lab, $parameter);
+   $cek->execute();
+   $result = $cek->get_result();
+
+   if ($result->num_rows > 0) {
+
+      // UPDATE
+      $stmt = $conn->prepare("
+            UPDATE hasil_lab 
+            SET hasil=?, satuan=?, referensi=? 
+            WHERE permintaan=? AND lab=? AND parameter=?
+        ");
+
+      $stmt->bind_param(
+         "ssssss",
+         $hasil,
+         $satuan,
+         $referensi,
+         $permintaan,
+         $lab,
+         $parameter
+      );
+   } else {
+
+      // INSERT
+      $stmt = $conn->prepare("
+            INSERT INTO hasil_lab
+            (permintaan,lab,parameter,hasil,satuan,referensi,create_at)
+            VALUES (?,?,?,?,?,?,NOW())
+        ");
+
+      $stmt->bind_param(
+         "ssssss",
+         $permintaan,
+         $lab,
+         $parameter,
+         $hasil,
+         $satuan,
+         $referensi
+      );
+   }
 
    $stmt->execute();
 }
